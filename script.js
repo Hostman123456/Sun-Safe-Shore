5948d7321889d170acd79b4f25c071ad6b72feda
maxisnotarichguy@gmail.com <65668267+Hostman123456@users.noreply.github.com>
2023-08-05 17:42:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let logo = document.getElementById("sun-safe")
+let nav = document.querySelector("nav")
+
+
+var funky_boi = new Audio('funky_yokai.mp3')
+
+funky_boi.play()
+
+
+
+
+
+logo.onclick = function() {
+    window.location='http://www.google.com';
+
+}
+
