9545eeddf772ffd005d98e6951552a0e60a15c68
maxisnotarichguy@gmail.com <65668267+Hostman123456@users.noreply.github.com>
2023-08-05 21:06:37 -0400

    Bout to try weird code for hover

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let logo = document.getElementById("sun-safe")
 let nav = document.querySelector("nav")
+let home_description = document.getElementById("home_description")
+let home_button = document.getElementById("home_button")
 
 
 var funky_boi = new Audio('funky_yokai.mp3')
