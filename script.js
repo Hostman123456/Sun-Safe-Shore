62c70fdd1935b5d75e6f3cb0f9764e8cd28fb0e5
maxisnotarichguy@gmail.com <65668267+Hostman123456@users.noreply.github.com>
2023-08-06 22:33:56 -0400

    Done with basics for home, can now just coast through the other windows

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let logo = document.getElementById("sun-safe")
 let nav = document.querySelector("nav")
 let home_description = document.getElementById("home_description")
 let home_button = document.getElementById("home_button")
+let cancer_types_description = document.getElementById("cancer_types_description")
+
+home_button.addEventListener("mouseenter", revealDescription )
+
+function revealDescription() {
+    home_description.style.visibility = "visible"
+}
 
 
 var funky_boi = new Audio('funky_yokai.mp3')
@@ -9,11 +16,21 @@ var funky_boi = new Audio('funky_yokai.mp3')
 funky_boi.play()
 
 
+function cancer_types_href() {
+    window.open("hub_copy.html")
+}
 
+
+function showCancerDescription() {
+    cancer_types_description.style.display = "inline"
+}
 
 
 logo.onclick = function() {
     window.location='http://www.google.com';
 
 }
+
+
+
 
