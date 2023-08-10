373b8ef79703c0caa634a723f5b8758c15aec284
maxisnotarichguy@gmail.com <65668267+Hostman123456@users.noreply.github.com>
2023-08-10 17:52:51 -0400

    Made it all organized, also added beginnings of quiz

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ let cancer_types_button = document.getElementById("cancer_types_button")
 let melanoma_description = document.getElementById("melanoma-description")
 let basal_description = document.getElementById("basal-cell-carcinoma-description")
 let merkel_description = document.getElementById("merkel-cell-carcinoma-description")
+let quiz_button = document.getElementById("quiz_button")
 
 cancer_types.style.display = "none"
 
-var funky_boi = new Audio('funky_yokai.mp3')
+var funky_boi = new Audio('audio/funky_yokai.mp3')
 funky_boi.loop = true
 
 funky_boi.play()
@@ -44,16 +45,22 @@ function toggleMerkelDescription() {
     }
 }
 
-cancer_types_button.onclick = function() {
-    home.style.display = "none"
-    cancer_types.style.display = "inline"
-}
+
 
 home_button.onclick = function() {
     cancer_types.style.display = "none"
     home.style.display = "flex"
 }
 
+cancer_types_button.onclick = function() {
+    home.style.display = "none"
+    cancer_types.style.display = "inline"
+}
+
+
+quiz_button.onclick = function() {
+    window.location = "quiz/quiz-index.html"
+}
 
 logo.onclick = function() {
     window.location='http://www.google.com';
